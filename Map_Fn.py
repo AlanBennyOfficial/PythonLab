8833fc5f453b5f0e83979622bc385b0d62e566f4
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-03-12 15:54:15 +0530

    Added files

--- Map_Fn.py
+++ Map_Fn.py
@@ -2,3 +2,7 @@ numbers = [1, 2, 3, 4, 5]
 exponent = [1, 2, 3, 4, 5]
 powered = list(map(pow, numbers, exponent))
 print(powered)
+
+# Map function with custom function
+custom = list(map(lambda x, y: x + y, numbers, exponent))
+print(custom)
