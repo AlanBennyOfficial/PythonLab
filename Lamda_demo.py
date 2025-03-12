bbc7eea7b939bbb912b3dc43acb0d2b23e49d2e1
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-03-12 14:25:33 +0530

    Modified files

--- Lamda_demo.py
+++ Lamda_demo.py
@@ -6,3 +6,8 @@ print(square(5))
 lst = [1,2,3,4,5]
 squared = list(map(lambda x: x ** 2, lst))
 print(squared)
+
+# Filter with lambda
+numbers = [1,2,3,4,5]
+evens = list(filter(lambda x: x % 2 == 0, numbers))
+print(evens)
