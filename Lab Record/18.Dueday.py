d26be735401316b42c6cfc7cdc6be006879465c4
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-04-25 19:13:31 +0530

    Added files via git

---
+++ Lab Record/18.Dueday.py
@@ -0,0 +1,13 @@
+due_days = int(input("Enter number of days late: ")) 
+
+if due_days < 0: 
+    print("Days late cannot be negative!") 
+    fine = 0  # Assign a default value to avoid NameError
+elif due_days <= 5: 
+    fine = due_days * 2 
+elif due_days <= 10: 
+    fine = due_days * 5 
+else: 
+    fine = due_days * 10 
+
+print("Total fine is:", fine)
