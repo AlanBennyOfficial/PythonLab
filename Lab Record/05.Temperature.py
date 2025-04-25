b252888ef3184d107b9f006b31bb377c78c93069
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-04-25 22:27:11 +0530

    Added files via git

--- Lab Record/05.Temperature.py
+++ Lab Record/05.Temperature.py
@@ -1,8 +1,8 @@
 t = float(input("Temp: "))
 
-unit = input("C or F? ").upper()
+unit = input("°C or °F? ").upper()
 
 if unit == 'C':
-    print("F =", t * 9/5 + 32)
+    print("°F =", t * 9/5 + 32)
 else:
-    print("C =", (t - 32) * 5/9)
+    print("°C =", (t - 32) * 5/9)
