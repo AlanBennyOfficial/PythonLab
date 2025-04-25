9067633880bcdfbbc5f1e9dc8bcfeafef9786129
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-04-25 15:34:52 +0530

    Added files via git

---
+++ Lab Record/15.Location.py
@@ -0,0 +1,15 @@
+def get_loc(city):
+    loc = [("new york", (40.71385, -73.99191)), ("paris", (48.8575, 2.3514)), ("LA", (34.0549, 118.2426))]
+    for name, coordinates in loc:
+        if name.lower() == city.lower():
+            return coordinates
+    return None
+
+
+city = input("name:")
+coordinates = get_loc(city)
+
+if coordinates:
+    print(f"{city} located in {coordinates}")
+else:
+    print(f"{city} not found")
